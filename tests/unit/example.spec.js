114560f909c7527f75e313a65b319a7b929aeb9b
zeyadMoamen <zeyad.moamen@hamza.solutions>
2023-06-07 18:20:36 +0300

    Fix bug in jest-watcher

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+import Home from "@/views/HomeView.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = mount(Home);
+    expect(wrapper.text()).toContain("Hello");
   });
 });
